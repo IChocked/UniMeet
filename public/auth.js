003aa9667c705d8a6e9952cf454f38d44c1d24f9
Brian Hardell <brian@segmed.ai>
2021-03-27 12:57:41 -0700

    - Basic registration - basic login - basic writing to database for registration
    Testing:
    	- basic functinoality of each function. NOT rigorous

---
+++ public/auth.js
@@ -0,0 +1,50 @@
+/*
+DOCS:
+- Firebase.User: https://firebase.google.com/docs/reference/js/firebase.User
+- firebase.auth.Error: https://firebase.google.com/docs/reference/js/firebase.auth.Error
+
+*/
+
+
+
+// @Param: email is a valid email, pass is a valid password according to Firebase requirements
+function register(email, pass) {
+  firebase.auth().createUserWithEmailAndPassword(email, pass)
+  .then((userCredential) => {
+    console.log("Success: register and sign in")
+     let user = userCredential.user;
+  })
+  .catch((error) => {
+    console.log(error.code)
+    console.log(error.message)
+    console.log(error)
+  })
+}
+
+// @Param: email, password
+function login(email, pass) {
+  firebase.auth().signInWithEmailAndPassword(email, pass)
+  .then((userCredential) => {
+    console.log("Success: log in")
+     let user = userCredential.user;
+  })
+  .catch((error) => {
+    console.log(error.code)
+    console.log(error.message)
+    console.log(error)
+  })
+  .finally(() => {
+    populateUser("UCLA", "Brian", ["Climbing", "Climbing", "Climbing", "Climbing", "Climbing"])
+  })
+}
+
+
+function logout() {
+  firebase.auth().signOut().then(() => {
+    console.log("Success: Sign Out")
+  }).catch((error) => {
+    console.log(error.code)
+    console.log(error.message)
+    console.log(error)
+  });
+}
--- public/db.js
+++ public/db.js
@@ -2,11 +2,40 @@
 TODO
 	- figure out reading and writing to db
 	- figure out basic authentication
+  - potentially we could populate the db with valid universities, but for hackathon purposes might not be necessary and we could use a single school as an example
+
+  Server-Side
+  - Take request with a user token and return a matched user token
+  - Take request with a string and return <=10 item array of interests with that started typed part
+  - Take request with a string and return <=10 item array of universities
+
+  - maintain a working live copy of the db in memory without retrieving the entire db on every new addition
 */
 
+// @param: potential uni
+// @description: make sure that the uni entered exists in the db to avoid single person uni's that won't have any matches
+function validateUni(uni) {}
 
-// authentication
-// function login() {}
-// function logout() {}
+// @Param: valid university (a key in the db), name: string, interests: string array
+function populateUser(uni, name, interests) {
+  let user = auth.currentUser
+  database.ref("Universities/" + uni + "/" + user.uid).set({
+    Name: name,
+    Interests: {
+      0: interests[0],
+      1: interests[1],
+      2: interests[2],
+      3: interests[3],
+      4: interests[4]
+    }
+  })
+}
 
-//
+login("bharde@me.com", "temP123@")
+
+// let values = login("bharde@me.com", "temP123@")
+// let user = values[0]
+// let err = values[1]
+
+// console.log(user)
+// console.log(err)
