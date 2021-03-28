04ee21c96a23fe9295e72e6817bea88e5c3498f7
Suleyman Eminov <suleyman.eminov22@gmail.com>
2021-03-28 03:21:59 -0400

    updated code

--- match/match.py
+++ match/match.py
@@ -1,82 +1,102 @@
 import json
 
-# Get the userID and University name
-USER_ID = "UID3"  # TODO: connect to db
-USER_UNI = "UCLA"  # TODO: connect to db
-
 
 def main():
     # open the file
     with open("data.json", "r") as read_file:  # TODO: connect to db
         data = json.load(read_file)
 
-    # provided data from the user: interests, name, [previous matches]
-    user_data = data["Universities"][USER_UNI][USER_ID]
-
+    # Get the userID and University name
+    user_id = "UID"  # TODO: connect to db
+    user_uni = data["users"][user_id]["university"]
     # all users of the university
-    uni_users = data["Universities"][USER_UNI]
+    uni_users = get_uni_users(data, user_id, user_uni)
 
-    # Keep track of points for each user
-    match_points = {}
+    print(uni_users)
+    uni_users = previously_matched(data, user_id, uni_users)
+    print(uni_users)
 
-    print(user_data["PreviousMatches"])
+    best_match = match(uni_users, user_id, data)
 
-    max_user = -1
-    user_ids = []
-    for i in uni_users:
-        max_user += 1
-        user_ids.append(i)
-    print(max_user)
+    update_data()
+    print(best_match)
 
-    """
-    This function loops over the data of user's university and 
-    assigns points for each of the other users 
-    """
 
-    def match():
-        points = 0  # increment when interests match
-        for user in uni_users:  # loop over the whole json file. i is user id
-            if USER_ID != user:  # exclude main user's data for the next loop
-                # check if previously matched
-                matched_before = previously_matched(user)
-                if not matched_before:
-                    print("not matched")
-                    # loop over the other user's interests
-                    for j in data["Universities"][USER_UNI][user]["Interests"]:
-                        # check if interests match
-                        try:
-                            if user_data["Interests"][j] == 1 and uni_users[user]["Interests"][j] == 1:
-                                print("H", data["Universities"][USER_UNI][user]["Interests"][j])
-                                points += 1
-                        except KeyError:
-                            pass
+"""
+Checks if users matched before
+"""
 
-                    # Assign points for each user in the uni
-                    match_points[user] = points
-        print("This", match_points)
 
-    # TODO: create this function
-    def find_the_best_match():
-        for index in match_points:
-            pass
+def previously_matched(data, user_id, users):
+    try:
+        for m in data["users"][user_id]["Meetings"]:
+            users.remove(m)
+    except ValueError:
+        pass
 
-    """
-    Checks if users matched before
-    """
-    def previously_matched(user_id):
-        print("user id", user_id)
-        for n in range(max_user):
+    return users
+
+
+# """
+# This function loops over the data of user's university and
+# assigns points for each of the other users
+#
+# """
+def match(users, id, data):
+    user_points = {}
+    points = 0  # increment when interests match
+    index = 0
+    user_interests = get_user_interests(data,id)
+    for user in users: # i is another user
+        for interest in user_interests:
             try:
-                if user_data["PreviousMatches"][str(n)] == user_id:
-                    return True
+                if interest == data["users"][user]["interests"][str(index)]:
+                    points += 1
+                user_points[user] = points
             except KeyError:
-                print(Exception)
-        print("here")
-        return False
+                pass
 
-    match()
+            index += 1
+        index = 0
+        points = 0
+    print(user_points)
 
-    # TODO: create a function that updates the PreviousMatches after users match
+    our_match = find_the_best_match(user_points)
+
+    return our_match
+
+
+# Chooses the max value user that was calculated on match()
+def find_the_best_match(u_points):
+    highest_point = max(u_points,key=u_points.get)
+    return highest_point
+
+
+def get_uni_users(json_data, user_id, user_uni):
+    users = []
+    for user in json_data["users"]:
+        if user != user_id:
+            if json_data["users"][user]['university'] == user_uni:
+                users.append(user)
+    return users
+
+
+def get_user_interests(data,user):
+    max_interests = 0
+    interests = []
+    for _ in (data["users"][user]["interests"]):
+        max_interests += 1
+
+    for i in range(max_interests):
+        interests.append(data["users"][user]["interests"][str(i)])
+    print(interests)
+    return interests
+
+
+# Updates json data
+def update_data():
+    pass # TODO update data
 
 
 main()
+
